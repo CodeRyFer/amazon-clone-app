07b856668fc9e7150ff11e7e955ba4994aeb6ee3
Ryan Fehr <coderry4@gmail.com>
2025-12-02 18:56:20 -0600

    feat: Add core Firebase SDK initialization

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDUwtV-ys61EYunguUVIQXCEowHLbcZEW0",
+  authDomain: "clone-app-96642.firebaseapp.com",
+  projectId: "clone-app-96642",
+  storageBucket: "clone-app-96642.firebasestorage.app",
+  messagingSenderId: "116664492624",
+  appId: "1:116664492624:web:77001a308f36e373a76755",
+  measurementId: "G-BRBHCK25V4"
+};
