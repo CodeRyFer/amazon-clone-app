9d0cd22e4e79f1e000a6f06b5a5682bc594530c0
Ryan Fehr <coderry4@gmail.com>
2025-12-07 12:34:50 -0600

    feat: Add initial home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     // BEM
     <div className="app">
      <Header />
+     <Home />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,47 @@
 import React from 'react'
 import './Header.css'
 import logo from './logo.png';
+import SearchIcon from '@mui/icons-material/Search';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 function Header() {
   return (
     <div className='header'>
         <img className='header__logo' src={logo} alt='Amazon Logo'></img>
+
+        <div className='header__deliveryLocation'>
+            <LocationOnOutlinedIcon className='header__locationIcon'></LocationOnOutlinedIcon>
+            <div className='header__locationText'>
+                <span className='header__locationLineOne'>Delivering to Bloomington 61704</span>
+                <span className='header__locationLineTwo'>Update Location</span>
+            </div>
+        </div>
     
         <div className='header__search'>
             <input className='header__searchInput' type='text'/>
-            {/*Search Logo */}
+            <SearchIcon className='header__searchIcon'></SearchIcon>
         </div>
 
         <div className='header__nav'>
             <div className='header__option'>
                 <span className='header__optionLineOne'>Hello</span>
-                <span className='header__optionLineOne'>Sign in</span>
+                <span className='header__optionLineTwo'>Sign in</span>
             </div>
 
             <div className='header__option'>
                 <span className='header__optionLineOne'>Returns</span>
-                <span className='header__optionLineOne'>& Orders</span>
+                <span className='header__optionLineTwo'>& Orders</span>
             </div>
 
             <div className='header__option'>
                 <span className='header__optionLineOne'>Your</span>
-                <span className='header__optionLineOne'>Prime</span>
+                <span className='header__optionLineTwo'>Prime</span>
+            </div>
+
+            <div className='header__optionBasket'>
+                <ShoppingCartOutlinedIcon />
+                <span className='header__optionLineTwo header__basketCount'>0</span>
             </div>
         </div>
 
