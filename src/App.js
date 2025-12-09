73087d5d947fdd0ad09ecd9b1bac1670af44cc1d
Ryan Fehr <coderry4@gmail.com>
2025-12-08 20:10:00 -0600

    Fix: Cleaned App.js routing functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     // BEM
     <Router>
-    <div className="app">
-     <Header />
-     <Home />
-    </div>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
