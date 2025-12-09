30e04062485a10816d75cf792b920e13ecf304a0
Ryan Fehr <coderry4@gmail.com>
2025-12-08 20:21:11 -0600

    feat: Add Checkout page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
@@ -13,6 +14,7 @@ function App() {
         <Header />
         <Routes>
           <Route path='/' element={<Home />}></Route>
+          <Route path='/checkout' element={<Checkout />}></Route>
         </Routes>
       </div>
     </Router>
