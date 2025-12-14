75a43f9e77c275230c45f4d8fe8cc96153cb918f
Ryan Fehr <coderry4@gmail.com>
2025-12-14 13:40:28 -0600

    feat: Add login page layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
+import Login from './Login';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
