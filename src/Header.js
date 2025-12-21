ecd135e11c72a132482e615af86dd52b7aada385
Ryan Fehr <coderry4@gmail.com>
2025-12-21 12:56:30 -0600

    chore: initialize Firebase hosting and deploy app

--- src/Header.js
+++ src/Header.js
@@ -35,7 +35,7 @@ function Header() {
          </div>
     
          <div className='header__nav'>
-            <Link to='/login'>
+            <Link to={!user && '/login'}>
              <div onClick={handleAuthentication} className='header__option'>
                  <span className='header__optionLineOne'>Hello</span>
                  <span className='header__optionLineTwo'>
